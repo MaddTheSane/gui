94ccec7cb20c6386398612bb261659b49a3537c2
C.W. Betts <computers57@hotmail.com>
2017-04-01 17:27:33 -0600

    More OS X/Cocoa interop.

--- macosx/GNUstepGUI/GNUstepGUI.h
+++ macosx/GNUstepGUI/GNUstepGUI.h
@@ -17,4 +17,54 @@ FOUNDATION_EXPORT const unsigned char GNUstepGUIVersionString[];
 #import <GNUstepGUI/GMArchiver.h>
 #import <GNUstepGUI/GMAppKit.h>
 
+#import <GNUstepGUI/GSGormLoading.h>
+
+#import <GNUstepGUI/GSNibContainer.h>
+#import <GNUstepGUI/GSNibLoading.h>
+
+#import <GNUstepGUI/GSXibElement.h>
+#import <GNUstepGUI/GSXibLoading.h>
+#import <GNUstepGUI/GSXibParser.h>
+#import <GNUstepGUI/GSXibObjectContainer.h>
+
+#import <GNUstepGUI/GSInstantiator.h>
+
+
+#import <GNUstepGUI/GSAnimator.h>
+#import <GNUstepGUI/GSCharacterPanel.h>
+//#import <GNUstepGUI/GSDisplayServer.h>
+//#import <GNUstepGUI/GSDragView.h>
+#import <GNUstepGUI/GSEPSPrintOperation.h>
+//#import <GNUstepGUI/GSFontInfo.h>
+#import <GNUstepGUI/GSGhostscriptImageRep.h>
+#import <GNUstepGUI/GSHbox.h>
+#import <GNUstepGUI/GSHelpAttachment.h>
+#import <GNUstepGUI/GSHelpManagerPanel.h>
+#import <GNUstepGUI/GSHorizontalTypesetter.h>
+#import <GNUstepGUI/GSImageMagickImageRep.h>
+#import <GNUstepGUI/GSInfoPanel.h>
+//#import <GNUstepGUI/GSLayoutManager.h>
+//#import <GNUstepGUI/GSLayoutManager_internal.h>
+#import <GNUstepGUI/GSMemoryPanel.h>
+//#import <GNUstepGUI/GSMethodTable.h>
+#import <GNUstepGUI/GSModelLoaderFactory.h>
+#import <GNUstepGUI/GSPasteboardServer.h>
+#import <GNUstepGUI/GSPDFPrintOperation.h>
+#import <GNUstepGUI/GSPrinting.h>
+#import <GNUstepGUI/GSPrintOperation.h>
+#import <GNUstepGUI/GSServicesManager.h>
+//#import <GNUstepGUI/GSSoundSink.h>
+//#import <GNUstepGUI/GSSoundSource.h>
+#import <GNUstepGUI/GSTable.h>
+#import <GNUstepGUI/GSTextConverter.h>
+//#import <GNUstepGUI/GSTheme.h>
+#import <GNUstepGUI/GSTitleView.h>
+#import <GNUstepGUI/GSToolbarView.h>
+#import <GNUstepGUI/GSTrackingRect.h>
+#import <GNUstepGUI/GSTypesetter.h>
+#import <GNUstepGUI/GSVbox.h>
+#import <GNUstepGUI/GSWindowDecorationView.h>
+#import <GNUstepGUI/IMConnectors.h>
+#import <GNUstepGUI/IMCustomObject.h>
+#import <GNUstepGUI/IMLoading.h>
 
