d56895cb2aa8b17bad321eb0716ae776f35d0c3e
C.W. Betts <computers57@hotmail.com>
2017-04-01 13:49:56 -0600

    Initial OS X framework.

---
+++ macosx/GNUstepGUI/GNUstepGUI.h
@@ -0,0 +1,19 @@
+//
+//  GNUstepGUI.h
+//  GNUstepGUI
+//
+//  Created by C.W. Betts on 4/1/17.
+//  Copyright © 2017 C.W. Betts. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for GNUstepGUI.
+FOUNDATION_EXPORT double GNUstepGUIVersionNumber;
+
+//! Project version string for GNUstepGUI.
+FOUNDATION_EXPORT const unsigned char GNUstepGUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GNUstepGUI/PublicHeader.h>
+
+
